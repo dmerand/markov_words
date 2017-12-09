ad33f551e3830db04c9eb71150399616d6a9e1e0
Donald Merand <dmerand@explo.org>
2017-12-09 15:21:11 -0500

    convert file_store to use SQLite as the engine

--- lib/markov_words/file_store.rb
+++ lib/markov_words/file_store.rb
@@ -1,39 +1,68 @@
+# frozen-string-literal: true
+
 require 'securerandom'
+require 'sqlite3'
 
 module MarkovWords
-  # Utility for persisting arbitrary data to disk as Marshal'ed Ruby objects
+  # Utility for persisting arbitrary data to disk.
   class FileStore
-    attr_reader :file_path
-    attr_reader :data
-
     # @option opts [String] :file_path Path and name for where the file should
     #   be stored.
     # @option opts [Boolean] :flush_data Do you want the file to be cleared on
     #   open?
     def initialize(opts)
       @file_path = opts.fetch :file_path, "/tmp/#{SecureRandom.base64}"
-      delete_if_exists(@file_path) if opts[:flush_data]
+      initialize_db
+      empty_db if opts[:flush_data]
     end
 
-    # Store arbitary data into file storage
+    # Store arbitary data, named with a `key`
+    # @param key [Symbol] Unique key for later data retrieval
     # @param data [Object] Any Marshal-able object
-    def store_data(data)
-      File.open(@file_path, 'wb') { |f| Marshal.dump(data, f) }
+    def store_data(key = :discard, data = nil)
+      key = key.to_s unless key.is_a? String
+      if data_exists(key)
+        @db.execute 'UPDATE data SET value = ? WHERE key = ?',
+                    [Marshal.dump(data), key]
+      else
+        @db.execute 'INSERT INTO data VALUES ( ?, ? )',
+                    [key, Marshal.dump(data)]
+      end
     end
 
-    # Retrieve whatever data is stored in the file + return it
-    def retrieve_data
-      result = nil
-      if File.exist?(@file_path)
-        File.open(@file_path, 'r') { |f| result = Marshal.load(f) }
-      end
-      result
+    # Retrieve whatever data is stored in at `key`, and return it!
+    def retrieve_data(key = '')
+      key = key.to_s unless key.is_a? String
+      data_array = @db.execute 'SELECT value FROM data WHERE key = ?', key
+      Marshal.load(data_array[0][0]) unless data_array[0].nil?
     end
 
     private
 
-    def delete_if_exists(path)
-      File.delete path if File.exist? path
+    def create_initial_tables
+      @db.execute <<~SQL
+        create table data (
+          key varchar(5),
+          value binary
+        );
+      SQL
+    end
+
+    def data_exists(key)
+      query = 'SELECT count(*) FROM data WHERE key = ?'
+      @db.execute(query, key)[0][0].positive?
+    end
+
+    def empty_db
+      @db.execute 'delete from data'
+    end
+
+    def initialize_db
+      @db = SQLite3::Database.new @file_path
+      table = @db.execute("SELECT name FROM sqlite_master
+                          WHERE type='table'
+                          AND name='data';").first
+      create_initial_tables unless table
     end
   end
 end
--- test/file_store_test.rb
+++ test/file_store_test.rb
@@ -1,51 +1,61 @@
+# frozen-string-literal: true
+
 require 'test_helper'
 require 'markov_words/file_store'
 
 class FileStoreTest < Minitest::Test
   def test_data_retrieval
     file_store = get_file_store
-    file_store.store_data(test_data)
-    assert_equal file_store.retrieve_data, test_data
+    file_store.store_data key, data
+    assert_equal file_store.retrieve_data(key), data
 
-    cleanup(file_store)
+    cleanup
   end
 
   def test_stores_to_passed_file
     file_path = 'tmp/test_custom_file_path.test'
     file_store = get_file_store(file_path: file_path)
-    file_store.store_data(test_data)
+    file_store.store_data(key, data)
 
     assert File.exist? file_path
 
-    cleanup(file_store)
+    cleanup file_path
   end
 
   def test_flush_file
     fs_one = get_file_store
-    fs_one.store_data test_data
+    fs_one.store_data key, data
     # now we have data in the file
     fs_two = get_file_store(flush_data: true)
     assert_nil fs_two.retrieve_data
 
-    cleanup(fs_one)
-    cleanup(fs_two)
+    cleanup
+    cleanup
   end
 
   private
 
-  def cleanup(file_store)
-    File.delete(file_store.file_path) if File.exist?(file_store.file_path)
+  def cleanup(file_path = default_path)
+    File.delete(file_path) if File.exist?(file_path)
+  end
+
+  def default_path
+    'tmp/storage_test.db'
   end
 
   # Keep the n-gram size down for testing, because computation is faster that
   # way. We also don't want caching by default (that gets tested specifically).
   def get_file_store(opts = {})
     MarkovWords::FileStore.new({
-      file_path: 'tmp/file_storage.test'
+      file_path: default_path
     }.merge(opts))
   end
 
-  def test_data
+  def data
     { cool: 'beans' }
+  end
+
+  def key
+    :test
   end
 end
